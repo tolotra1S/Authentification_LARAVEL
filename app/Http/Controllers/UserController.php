641631437b9a495aa84c77524b6a13896c58d091
tolotra1S <tolotraranarison@gmail.com>
2023-01-19 15:30:03 +0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    public function inscription(Request $request){
+        $utilisateurDonnee = $request -> validate([
+                "name" => ["required", "string", "min:2", "max:255"],
+                "email" => ["required", "email", "unique:users,email"],
+                "password" => ["required","string", "min:8", "max:30", "confirmed"]
+
+            ]);
+            $utilisateurs = User::create([
+                "name" => $utilisateurDonnee["name"],
+                "email" => $utilisateurDonnee["email"],
+                "password" => bcrypt($utilisateurDonnee["password"])
+            ]);
+            return response($utilisateurs,201);
+
+    }
+    public function connexion(Request $request) {
+        $utilisateurDonnee = $request -> validate([
+            "email" => ["required", "email"],
+            "password" => ["required","string", "min:8", "max:30"]
+
+        ]);
+        $utilisateur = User::where("email",$utilisateurDonnee["email"])->first();
+        if(!$utilisateur) return response(["message" => "Aucun utilisateur de trouver avec l'email suivante $utilisateurDonnee[email]"],401); 
+        /* if(!Hash::check($utilisateurDonnee["password"], $utilisateur -> password))
+        {
+        return response(["message" => "Aucun utilisateur de trouver avec ce mot de passe "],401);
+          } */
+        /* $token = $utilisateur->createToken("CLE_SECRETE")->plainTextToken;
+        return response([
+            "utilisateur" => $utilisateur,
+            "token" => $token,
+        ], 200); */
+        return $utilisateurs;
+    }
+}   
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get("/", function(){
-    return "Hello world";
-});
+Route::post("/utilisateur/inscription", [UserController::class, "inscription"]);
+Route::post("/utilisateur/connexion", [UserController::class, "connexion"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
